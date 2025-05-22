f6a4ddc73a34506b9b204dbb4acf7b7f56e3f3be
ritesh-metagame <ritesh@meta-game.ai>
2025-05-22 10:42:36 +0530

    feat: Add health check endpoint for lobby server

--- messaging/rest/rest.go
+++ messaging/rest/rest.go
@@ -12,6 +12,10 @@ func StartRESTApiServer(port string) error {
 
 	lobbyRouteHandler.HandleRoutes()
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Lobby server is live!")
+	})
+
 	log.Printf("Starting lobby server on port %s", port)
 	return http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
